2f705fa59c6b91d134a1ff18a0d5bfd8561a3d95
Loïc Herr <loic@herrloc.fr>
2020-01-31 10:47:23 +0100

    Style bannière haut

---
+++ template-parts/header-top.php
@@ -0,0 +1,45 @@
+<header class="header-connected">
+
+	<input type="checkbox" id="displayMenu" class="display-menu">
+
+
+	<button class="arrow left-arrow">
+		<span class="arrow-text">Retour</span>
+		<span class="arrow-shape"></span>
+	</button>
+
+	<button class="bt header-toggle border-white-bt" for="displayMenu">
+		<label for="displayMenu">
+			<span class="displayMenuOuvert">Fermer</span>
+			<span class="displayMenuFerme">Mes listes</span>
+		</label>
+	</button>
+
+
+	<ul class="multi-user">
+		<li >
+		<a href="" class="single-people">
+			<span>Anne-Laure</span>
+			<img src="src/img/avatar/avatar10.png" alt="">
+			</a>
+		</li>
+		<li >
+		<a href="" class="single-people">
+			<span>Anne-Laure</span>
+			<img src="src/img/avatar/avatar10.png" alt="">
+			</a>
+		</li><li >
+		<a href="" class="single-people">
+			<span>Anne-Laure</span>
+			<img src="src/img/avatar/avatar10.png" alt="">
+			</a>
+		</li><li >
+		<a href="" class="single-people">
+			<span>Anne-Laure</span>
+			<img src="src/img/avatar/avatar10.png" alt="">
+			</a>
+		</li>
+
+	</ul>
+	
+</header>
--- user.php
+++ user.php
@@ -26,13 +26,7 @@
 
 <body class="connected-user-profil">
 
-	<header class="header-connected">
-		<button class="arrow left-arrow">
-			<span class="arrow-text">Retour</span>
-			<span class="arrow-shape"></span>
-		</button>
-		<a href="" class="bt border-white-bt">Mes listes</a>
-	</header>
+	<?php include('template-parts/header-top.php');?>
 
 	<?php if($active_user): ?>
 
