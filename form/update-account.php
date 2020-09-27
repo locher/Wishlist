8a6cc0787652cadc19f8f0dcb6d4382907854efa
Loïc Herr <loic@herrloc.fr>
2020-09-27 18:10:03 +0200

    Sanitize account edition

--- form/update-account.php
+++ form/update-account.php
@@ -1,42 +1,42 @@
 <?php
 
-// Gestion form
+// Form management
 
 if(isset($_POST['firstname']) && $_POST['firstname'] != ''){
-	$firstname = $_POST['firstname'];
+	$firstname = filter_var($_POST['firstname'], FILTER_SANITIZE_STRING);
 }
 
 if(isset($_POST['birthday']) && $_POST['birthday'] != ''){
-	$birthday = $_POST['birthday'];
+	$birthday = filter_var($_POST['birthday'], FILTER_SANITIZE_STRING);
 }else{
 	$birthday = NULL;
 }
 
 if(isset($_POST['size-top']) && $_POST['size-top'] != ''){
-	$sizeTop = $_POST['size-top'];
+	$sizeTop = filter_var($_POST['size-top'], FILTER_SANITIZE_STRING);
 }else{
 	$sizeTop = NULL;
 }
 
 if(isset($_POST['size-bottom']) && $_POST['size-bottom'] != ''){
-	$sizeBottom = $_POST['size-bottom'];
+	$sizeBottom = filter_var($_POST['size-bottom'], FILTER_SANITIZE_STRING);
 }else{
 	$sizeBottom = NULL;
 }
 
 if(isset($_POST['size-feet']) && $_POST['size-feet'] != ''){
-	$sizeFeet = $_POST['size-feet'];
+	$sizeFeet = filter_var($_POST['size-feet'], FILTER_SANITIZE_STRING);
 }else{
 	$sizeFeet = NULL;
 }
 
 if(isset($_POST['photoChoice']) && $_POST['photoChoice'] != ''){
-	$photoChoice = $_POST['photoChoice'];
+	$photoChoice = filter_var($_POST['photoChoice'], FILTER_SANITIZE_NUMBER_INT);
 }else{
 	$photoChoice = rand(1, 15);
 }
 
-if(isset($_POST['isChild']) && $_POST['isChild'] != ''){
+if(isset($_POST['choiceParent']) && $_POST['choiceParent'] != ''){
 	$isChild = 1;
 }else{
 	$isChild = 0;
