68d245dbe3756b70ced81dd90242cad67f9d683e
Loïc Herr <loic@herrloc.fr>
2020-09-29 21:58:45 +0200

    Confirmation on delete gift button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,4 +11,28 @@ $(document).ready(function() {
 		$(this).parent().find('.closed').toggleClass('closed');
 	});
 
+	//Edit gift
+
+	$('.edit-gift').on('click', function(){
+		var giftID = $(this).attr('data-giftid');
+		var giftName = $(this).parent().parent().find('h3').text();
+		var giftUrl = $(this).parent().parent().find('a').attr('href');
+		var giftDescription = $(this).parent().parent().find('.gift-description p').text();
+		console.log(giftID);
+		
+		$('.gift-form').toggleClass('closed');
+		$('.gift-form').find('[name="designation"]').val(giftName);
+	});
+
+
+	//Delete gift
+
+	$('.bt-delete-gift').on('click',function(){
+		$(this).parent().parent().find('.overlay-delete-gift').toggleClass('open');
+	});
+
+	$('.close-delete-bt-overlay').on('click',function(){
+		$(this).parent().parent().toggleClass('open');
+	})
+
 });
