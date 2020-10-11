01322601f52b0886711be51180035378c8b8b660
Loïc Herr <loic@herrloc.fr>
2020-10-11 18:12:09 +0200

    php code sniffer + fix index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 $_SESSION['userID'] = null;
 session_destroy();
 
-include_once('inc/header.php');
+require_once 'inc/header.php';
 
 $context['users'] = getUsers('parents');
 
@@ -12,24 +12,23 @@ $context['users'] = getUsers('parents');
 
 // Popup after account creation
 
-if(isset($_GET['src']) && isset($_GET['user']) && $_GET['user'] != ''){
+if (isset($_GET['src']) && isset($_GET['user']) && $_GET['user'] != '') {
 
-	if($_GET['src'] == 'CreateAccountOk'){
+    if ($_GET['src'] == 'CreateAccountOk') {
 
-		$context['pagetype'] = array(
-			'type' => 'creation',
-			'user' => $_GET['user']
-		);
+        $context['pagetype'] = array(
+        'type' => 'creation',
+        'user' => $_GET['user']
+        );
 
-	}else if($_GET['src'] == 'DeleteAccountOk'){
+    } else if ($_GET['src'] == 'DeleteAccountOk') {
 
-		$context['pagetype'] = array(
-			'type' => 'delete',
-			'user' => $_GET['user']
-		);
-	}
+        $context['pagetype'] = array(
+        'type' => 'delete',
+        'user' => $_GET['user']
+        );
+    }
 }
 
 // Render
-
 echo $twig->render('templates/frontpage.twig', $context);
