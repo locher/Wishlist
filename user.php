7d20c8483b7964c6ea602a7152674073e2abacc3
Loïc Herr <loic@herrloc.fr>
2020-10-12 19:46:05 +0200

    Back to profile button

--- user.php
+++ user.php
@@ -1,16 +1,16 @@
 <?php 
 
-include_once('inc/header.php');
+require_once 'inc/header.php';
 
 /////////////////////////////////////////
 // SESSION & PERMISSIONS 
 /////////////////////////////////////////
 
-if(isset($_GET['user']) && !isset($_SESSION['userID'])){
+if (isset($_GET['user']) && !isset($_SESSION['userID'])) {
     $_SESSION['userID'] = $_GET['user'];
 }
 
-if(isset($_SESSION['userID'])){
+if (isset($_SESSION['userID'])) {
     $logedInUser = $_SESSION['userID'];
 }else{
     header('location:index.php');
@@ -25,7 +25,7 @@ $permissions = array(
     'parent' => false,
 );
 
-if(isset($_GET['user']) && $_GET['user']){
+if (isset($_GET['user']) && $_GET['user']) {
     $currentUser = $_GET['user'];
 }
 
@@ -41,15 +41,15 @@ $currentUserGifts = getGifts($currentUser);
 
 //Check if is owner
 
-if(isset($_SESSION['userID']) && $logedInUser == $currentUser){
+if (isset($_SESSION['userID']) && $logedInUser == $currentUser) {
     $permissions['owner'] = true;
 }
 
 //Check if it's a child page
 
-if($logedInUserChildrenList != null){
+if ($logedInUserChildrenList != null) {
     foreach ($logedInUserChildrenList as $childrenID) {
-        if($childrenID == $currentUser){
+        if ($childrenID == $currentUser) {
             $permissions['parent'] = true;
         }
     }
@@ -66,7 +66,7 @@ $currentUserArray = array_filter(
     }
 );
 
-foreach($currentUserArray as $user){
+foreach ($currentUserArray as $user){
     $currentUserInfos = $user;
 }
 
@@ -75,7 +75,7 @@ foreach($currentUserArray as $user){
 // USER CHILDREN (IF HE HAS SOME)
 /////////////////////////////////////////
 
-if($currentUserChildrenList != null){
+if ($currentUserChildrenList != null) {
     //Get children infos
 
     $childrenListInfos = array_filter(
@@ -91,13 +91,13 @@ if($currentUserChildrenList != null){
 /////////////////////////////////////////
 
 //If the gift is reserved, attached informations of the reserv-er
-foreach($currentUserGifts as $key=>$gift){
-    if($gift['isReserved'] == true && $gift['reservationUserID'] != ''){
+foreach ($currentUserGifts as $key=>$gift){
+    if ($gift['isReserved'] == true && $gift['reservationUserID'] != '') {
 
         $currentUserGifts[$key]['reservation']['user'] = getUserInfo($gift['reservationUserID']);
 
         //If the loged user is the person who reserved the gift
-        if($logedInUser == $gift['reservationUserID']){
+        if ($logedInUser == $gift['reservationUserID']) {
             $currentUserGifts[$key]['reservation']['currentUser'] = true;
         }
     }
@@ -120,21 +120,21 @@ $otherUsers = array_filter(
 /////////////////////////////////////////
 
 //Gift just added message
-if(isset($_GET['statut']) && isset($_GET['gift']) && $_GET['statut'] == 'giftAdded' && $_GET['gift'] != ''){
-	$context['message']['object'] = getGift(filter_var($_GET['gift'], FILTER_SANITIZE_NUMBER_INT))['title'];
-	$context['message']['prefixe'] = 'Le cadeau';
-	$context['message']['suffixe'] = 'a bien été ajouté !';
+if (isset($_GET['statut']) && isset($_GET['gift']) && $_GET['statut'] == 'giftAdded' && $_GET['gift'] != '') {
+    $context['message']['object'] = getGift(filter_var($_GET['gift'], FILTER_SANITIZE_NUMBER_INT))['title'];
+    $context['message']['prefixe'] = 'Le cadeau';
+    $context['message']['suffixe'] = 'a bien été ajouté !';
 }
 
 //Unreserved gift
-if(isset($_GET['statut']) && isset($_GET['gift']) && $_GET['statut'] == 'giftUnreserved' && $_GET['gift'] != ''){
+if (isset($_GET['statut']) && isset($_GET['gift']) && $_GET['statut'] == 'giftUnreserved' && $_GET['gift'] != '') {
     $context['message']['object'] = getGift(filter_var($_GET['gift'], FILTER_SANITIZE_NUMBER_INT))['title'];
     $context['message']['prefixe'] = 'La réservation du cadeau';
     $context['message']['suffixe'] = 'a bien été supprimée !';
 }
 
 //Reserved gift
-if(isset($_GET['statut']) && isset($_GET['gift']) && $_GET['statut'] == 'giftReserved' && $_GET['gift'] != ''){
+if (isset($_GET['statut']) && isset($_GET['gift']) && $_GET['statut'] == 'giftReserved' && $_GET['gift'] != '') {
     $context['message']['object'] = getGift(filter_var($_GET['gift'], FILTER_SANITIZE_NUMBER_INT))['title'];
     $context['message']['prefixe'] = 'Le cadeau';
     $context['message']['suffixe'] = 'a bien été réservé !';
@@ -153,7 +153,7 @@ $context['logedInUser'] = $logedInUser;
 $context['currentUser']['infos'] = $currentUserInfos;
 $context['currentUser']['gifts'] = $currentUserGifts;
 
-if(isset($childrenListInfos)){
+if (isset($childrenListInfos)) {
     $context['children'] = $childrenListInfos;
 }
 
